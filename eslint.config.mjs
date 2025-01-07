df72a3070fa9be499131cdc10d09ee8d03bc2db8
oocon <86552440+oocon@users.noreply.github.com>
2025-01-07 20:34:39 +0100

    more changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,27 +6,36 @@ import pluginReact from "eslint-plugin-react";
 export default [
   {
     files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-  },
-  {
     languageOptions: {
       globals: globals.browser,
+      ecmaVersion: 2020,
+      sourceType: "module",
     },
-  },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  {
-    extends: ["next/core-web-vitals", "prettier"],
-    plugins: ["@typescript-eslint"],
+    parser: "@babel/eslint-parser",
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: "module",
+    },
     rules: {
       "no-unused-vars": [
-        "off", // Options: "off", "warn", or "error"
+        "off",
         {
-          argsIgnorePattern: "^_", // Ignore arguments starting with "_"
-          varsIgnorePattern: "^_", // Ignore variables starting with "_"
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
         },
       ],
       "@typescript-eslint/no-unused-vars": "off",
     },
+    plugins: {
+      react: pluginReact,
+      "@typescript-eslint": tseslint,
+    },
+    extends: [
+      "next/core-web-vitals",
+      "prettier",
+      pluginJs.configs.recommended,
+      ...tseslint.configs.recommended,
+      pluginReact.configs.flat.recommended,
+    ],
   },
 ];
